e31d65783be46364ef2702841a87a998d04b8697
barg991 <barg991@gmail.com>
2022-03-13 10:09:10 +0200

    final push

--- mat.hpp
+++ mat.hpp
@@ -3,7 +3,7 @@
 #include<vector>
 using namespace std;
 namespace ariel{
-    vector<vector<char>> loop(vector<vector<char>> arr,int originalwidth, int originallength,int minwidth,int minlength,char c);
+    vector<vector<char>> loop(vector<vector<char>> mat,int originalwidth, int originallength,int minwidth,int minlength,char c);
     string mat(int w,int l,char c,char spacing);
 }
 
