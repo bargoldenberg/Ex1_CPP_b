2b89a94c76616d8a85bfd1b15a965347ca9fec7c
barg991 <barg991@gmail.com>
2022-03-15 12:17:24 +0200

    made main better

--- Main.cpp
+++ Main.cpp
@@ -5,16 +5,15 @@
 int main(){
     cout<<
 "___  ___  ___ _____  ______ _____ _____ _____ _____  _   _  ___________  \n"
-"|  \\/  | / _ \\_   _| |  _  \\  ___/  ___|_   _|  __ \\| \\ | ||  ___| ___ | \n"
+"|  \\/  | / _ \\_   _| |  _  \\  ___/  ___|_   _|  __ \\| \\ | ||  ___| ___ \\ \n"
 "| .  . |/ /_\\ \\| |   | | | | |__ \\ `--.  | | | |  \\/|  \\| || |__ | |_/ / \n"
 "| |\\/| ||  _  || |   | | | |  __| `--. \\ | | | | __ | . ` ||  __||    /  \n"
 "| |  | || | | || |   | |/ /| |___/\\__/ /_| |_| |_\\ \\| |\\  || |___| |\\ \\  \n"
 "\\_|  |_/\\_| |_/\\_/   |___/ \\____/\\____/ \\___/ \\____/\\_| \\_/\\____/\\_| \\_| \n";
-                                                                        
-                                                                    
-    cout<<"Welcome to the mat designer..."<<endl;
+
+    cout<<endl;                                                                                                                                     
+    cout<<"Welcome to the mat designer..."<<endl<<endl;
     while(true){
-        
         int w;
         int l;
         char c;
@@ -22,29 +21,29 @@ int main(){
         cout<<"Enter mat witdh (MUST BE ODD): ";
         cin>>w;
         cout<<endl;
-         cout<<"Enter mat length (MUST BE ODD): ";
-        cin>>l;
-        cout<<endl;
-         cout<<"Enter first design (any printable char): ";
-        cin>>c;
-        cout<<endl;
-         cout<<"Enter second design (any printable char): ";
-        cin>>spacing;
-        cout<<endl;
         if(w%2==0){
             cout<< "Error! width must be odd."<<endl;
             continue;
         }
+         cout<<"Enter mat length (MUST BE ODD): ";
+        cin>>l;
+        cout<<endl;
         if(l%2==0){
             cout<< "Error! length must be odd."<<endl;
             continue;
         }
+        cout<<"Enter first design (any printable char): ";
+        cin>>c;
+        cout<<endl;
         if(std::isprint(c)==0){
              cout<< "Error! first char must be printable."<<endl;
             continue;
         }
-          if(std::isprint(spacing)==0){
-             cout<< "Error! second char must be printable."<<endl;
+        cout<<"Enter second design (any printable char): ";
+        cin>>spacing;
+        cout<<endl;
+        if(std::isprint(spacing)==0){
+            cout<< "Error! second char must be printable."<<endl;
             continue;
         }
         cout<<ariel::mat(w,l,c,spacing)<<endl;
